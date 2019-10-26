10ea5575f6f77052d6123388133a4d83ebc7804b
Juan German Arriaza Posada <juan.german.arriaza@gmail.com>
2019-10-26 16:40:52 +0200

    first commit

---
+++ service-a/src/main/java/es/german/microprile/demo/DemoRestApplication.java
@@ -0,0 +1,11 @@
+package es.german.microprile.demo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class DemoRestApplication extends Application {
+}
